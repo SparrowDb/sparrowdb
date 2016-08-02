8cbc7148d7aea7f51afcf096af85492eac2798eb
mausimag <mmmauriciosilvam@gmail.com>
2016-08-02 18:32:03 -0300

    change tombstone status

--- model/tombstone.go
+++ model/tombstone.go
@@ -1,10 +1,10 @@
 package model
 
 // NewTombstone returns new DataDefinition
-// Tombstones are DataDefinition with Status = 2
+// Tombstones are DataDefinition with Status = DataDefinitionRemoved
 // and empty byte buffer containing the image data
 func NewTombstone(df *DataDefinition) *DataDefinition {
-	df.Status = 2
+	df.Status = DataDefinitionRemoved
 	df.Buf = []byte("")
 	return df
 }
