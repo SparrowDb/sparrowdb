df7d178bf24b649418b9d940924d7f1e6146b0ff
Maurício Silva de Magalhães <mauricio.magalhaes@radixengrj.matriz>
2016-08-08 18:15:11 -0300

    fix windows storage

--- engine/file_storage_windows.go
+++ engine/file_storage_windows.go
@@ -1,14 +1,9 @@
-package engine
-
-
 // Copyright 2013 The LevelDB-Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package db
+package engine
 
 import (
-	"io"
 	"syscall"
 )
 
